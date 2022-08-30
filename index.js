50222ef67ff2c7046a487cde639c0704446d7356
francoalbiero <albierofranco@gmail.com>
2022-08-29 23:09:00 -0300

    Primera entrega js

---
+++ index.js
@@ -0,0 +1,8 @@
+let usuario = prompt("Ingrese tu nombre");{
+    
+    usuario = prompt("Ingresa tu apellido");
+    usuario = prompt("Ingresa tu edad");
+    usuario = prompt("Ingresa tu número celular sin 0 y sin 15");
+    usuario = prompt("Ingresa tu mail");
+}
+alert("Muchas gracias por confiar en mi, te llegará un correo con toda la información de tu turno!");
